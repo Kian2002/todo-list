a990e18c7c88b35aaa3cfe95d47386a086089afe
Kian2002 <canadakurdistan@gmail.com>
2022-12-17 01:10:59 -0800

    commented out a line, added to gitignore

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const _ = require("lodash");
-const { ATLAS } = require("./config"); // Uncomment this line if you are using a local config file
-//const ATLAS = process.env.ATLAS;
+//const { ATLAS } = require("./config"); // Uncomment this line if you are using a local config file
+const ATLAS = process.env.ATLAS;
 
 mongoose.set("strictQuery", true);
 
